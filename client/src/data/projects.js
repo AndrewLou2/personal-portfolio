ced258d1f96ba66efc349483d3eb4b6946655c52
AndrewLou2 <louandrew123@gmail.com>
2021-11-06 03:00:04 -0400

    added files

---
+++ client/src/data/projects.js
@@ -0,0 +1,45 @@
+import birrbot from "../images/birrbot.png";
+import portfolio from "../images/portfolio_website.png";
+import tob from "../images/ToB.png";
+import connect from "../images/connect-4.png";
+
+const portfolios = [
+    {
+        id: 3,
+        category: 'React JS',
+        image: portfolio,
+        link1: 'https://www.google.com',
+        link2: 'https://www.google.com',
+        title: 'Personal Portfolio Website',
+        text: 'Created with only HTML and CSS.'
+    },
+    {
+        id: 2,
+        category: 'Python',
+        image: birrbot,
+        link1: 'https://github.com/AndrewLou2/BirrBot-Discord',
+        link2: 'https://www.google.com',
+        title: 'Discord Server Bot',
+        text: 'Number one Animation Application'
+    },
+    {
+        id: 1,
+        category: 'Unity',
+        image: tob,
+        link1: 'https://github.com/JiyongSong1/SE2250_jsong386',
+        link2: 'https://www.google.com',
+        title: 'Thunder or Bust Game',
+        text: 'Number one Animation Application'
+    },
+    {
+        id: 4,
+        category: 'React JS',
+        image: connect,
+        link1: 'https://github.com/ShaimaaAliECE/lab2-AndrewLou2',
+        link2: 'https://www.google.com',
+        title: 'Connect 4/16 Web Game',
+        text: 'Free Animation Software'
+    },
+]
+
+export default portfolios;
