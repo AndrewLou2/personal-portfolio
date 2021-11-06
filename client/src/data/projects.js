8eedb45d15c3f5c53f436b431d4cd5a4c3cec75b
AndrewLou2 <louandrew123@gmail.com>
2021-11-06 03:15:18 -0400

    added project description

--- client/src/data/projects.js
+++ client/src/data/projects.js
@@ -4,42 +4,42 @@ import tob from "../images/ToB.png";
 import connect from "../images/connect-4.png";
 
 const portfolios = [
-    {
-        id: 3,
-        category: 'React JS',
-        image: portfolio,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Personal Portfolio Website',
-        text: 'Created with only HTML and CSS.'
-    },
-    {
-        id: 2,
-        category: 'Python',
-        image: birrbot,
-        link1: 'https://github.com/AndrewLou2/BirrBot-Discord',
-        link2: 'https://www.google.com',
-        title: 'Discord Server Bot',
-        text: 'Number one Animation Application'
-    },
-    {
-        id: 1,
-        category: 'Unity',
-        image: tob,
-        link1: 'https://github.com/JiyongSong1/SE2250_jsong386',
-        link2: 'https://www.google.com',
-        title: 'Thunder or Bust Game',
-        text: 'Number one Animation Application'
-    },
-    {
-        id: 4,
-        category: 'React JS',
-        image: connect,
-        link1: 'https://github.com/ShaimaaAliECE/lab2-AndrewLou2',
-        link2: 'https://www.google.com',
-        title: 'Connect 4/16 Web Game',
-        text: 'Free Animation Software'
-    },
-]
+  {
+    id: 3,
+    category: "React JS",
+    image: portfolio,
+    link1: "https://github.com/AndrewLou2/personal-portfolio",
+    link2: "https://andrewlou.herokuapp.com/",
+    title: "Personal Portfolio Website",
+    text: "A full-stack, fully-responsive personal portfolio website show-casing myself",
+  },
+  {
+    id: 2,
+    category: "Python",
+    image: birrbot,
+    link1: "https://github.com/AndrewLou2/BirrBot-Discord",
+    link2: "https://www.google.com",
+    title: "Discord Server Bot",
+    text: "A fun interactive discord bot that provides 140+ users with a unique experience",
+  },
+  {
+    id: 1,
+    category: "Unity",
+    image: tob,
+    link1: "https://github.com/JiyongSong1/SE2250_jsong386",
+    link2: "https://www.google.com",
+    title: "Thunder or Bust Game",
+    text: "Managed a team of five people with the development of a 2-D RPG game",
+  },
+  {
+    id: 4,
+    category: "React JS",
+    image: connect,
+    link1: "https://github.com/ShaimaaAliECE/lab2-AndrewLou2",
+    link2: "https://www.google.com",
+    title: "Connect 4/16 Web Game",
+    text: "Front-end connect-four game implementing hooks",
+  },
+];
 
 export default portfolios;
